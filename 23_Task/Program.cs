a7ea711cf2d09dfb4f4158a6bf28c45b328a0376
Администратор <@DESKTOP-PJPICF5>
2021-12-09 12:29:31 +0800

    Добавьте файлы проекта.

---
+++ 23_Task/Program.cs
@@ -0,0 +1,28 @@
+﻿
+static void Factorial()
+{
+    int result = 1;
+    for (int i = 1; i <= 10; i++)
+    {
+        result *= i;
+    }
+    Thread.Sleep(5000);
+    Console.WriteLine($"Факториал равен {result}");
+}
+
+static async void FactorialAsync()
+{
+    Console.WriteLine("Начало метода FactorialAsync"); 
+    await Task.Run(() => Factorial());                
+    Console.WriteLine("Конец метода FactorialAsync");
+}
+
+
+
+    FactorialAsync();
+
+    Console.WriteLine("Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Квадрат числа равен {n * n}");
+
+    Console.Read();
